035058c58c820718f988cce559f1fcb0e33bf6bf
jjlmoya <juanjo1989@gmail.com>
2026-04-18 21:28:54 +0200

    fix: remove re-exports of removed component vars from main index

--- src/index.ts
+++ src/index.ts
@@ -17,11 +17,11 @@ export type {
 
 export { ALL_ENTRIES, ALL_TOOLS } from './tools';
 
-export { QRGeneratorComponent, QRGeneratorSEO, QRGeneratorBibliography, QR_GENERATOR_TOOL } from './tool/qrGenerator';
-export { SolarCalculatorComponent, SolarCalculatorSEO, SolarCalculatorBibliography, SOLAR_CALCULATOR_TOOL } from './tool/solarCalculator';
-export { ProjectorCalculatorComponent, ProjectorCalculatorSEO, ProjectorCalculatorBibliography, PROJECTOR_CALCULATOR_TOOL } from './tool/projectorCalculator';
-export { DewPointCalculatorComponent, DewPointCalculatorSEO, DewPointCalculatorBibliography, DEW_POINT_CALCULATOR_TOOL } from './tool/dewPointCalculator';
-export { LedSavingCalculatorComponent, LedSavingCalculatorSEO, LedSavingCalculatorBibliography, LED_SAVING_CALCULATOR_TOOL } from './tool/ledSavingCalculator';
-export { TariffComparatorComponent, TariffComparatorSEO, TariffComparatorBibliography, TARIFF_COMPARATOR_TOOL } from './tool/tariffComparator';
-export { HeatingComparatorComponent, HeatingComparatorSEO, HeatingComparatorBibliography, HEATING_COMPARATOR_TOOL } from './tool/heatingComparator';
+export { QR_GENERATOR_TOOL } from './tool/qrGenerator';
+export { SOLAR_CALCULATOR_TOOL } from './tool/solarCalculator';
+export { PROJECTOR_CALCULATOR_TOOL } from './tool/projectorCalculator';
+export { DEW_POINT_CALCULATOR_TOOL } from './tool/dewPointCalculator';
+export { LED_SAVING_CALCULATOR_TOOL } from './tool/ledSavingCalculator';
+export { TARIFF_COMPARATOR_TOOL } from './tool/tariffComparator';
+export { HEATING_COMPARATOR_TOOL } from './tool/heatingComparator';
 
