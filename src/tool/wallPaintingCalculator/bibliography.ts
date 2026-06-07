7bd8195fa17072005e48910f5afab013333376a3
jjlmoya <juanjo1989@gmail.com>
2026-06-07 20:46:10 +0200

    fix: bibliography

--- src/tool/wallPaintingCalculator/bibliography.ts
+++ src/tool/wallPaintingCalculator/bibliography.ts
@@ -1,14 +1,14 @@
 export const bibliography = [
   {
-    name: 'Paint Coverage and Application Guide: Sherwin-Williams',
-    url: 'https://www.sherwin-williams.com/homeowners/how-to/paint-coverage-and-application',
+    name: 'Paint Coverage and Application Guide',
+    url: 'https://www.sherwin-williams.com/home-builders/color/color-education/sw-article-pro-hueandhide',
   },
   {
     name: 'How Much Paint Do I Need: Benjamin Moore',
-    url: 'https://www.benjaminmoore.com/en-us/how-to/how-much-paint-do-i-need',
+    url: 'https://www.benjaminmoore.com/en-us/paint-calculator',
   },
   {
     name: 'Paint Calculator and Coverage Estimator: Dulux',
-    url: 'https://www.dulux.co.uk/en/diy/paint-calculator',
+    url: 'https://www.dulux.co.uk/en/paint-calculator',
   },
 ];
