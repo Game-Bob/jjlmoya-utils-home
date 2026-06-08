bca6e7fec90fc1cdddba465b4d9237f2cfa8ea56
jjlmoya <juanjo1989@gmail.com>
2026-06-08 10:07:58 +0200

    fix: bibliography

--- src/tool/lightingCalculator/bibliography.ts
+++ src/tool/lightingCalculator/bibliography.ts
@@ -1,10 +1,10 @@
 export const bibliography = [
   {
     name: 'IES Lighting Handbook 10th Edition',
-    url: 'https://www.ies.org/publications/ies-lighting-handbook/',
+    url: 'https://www.usgbc.org/resources/lighting-handbook-10th-edition-illuminating-engineering-society-north-america',
   },
   {
     name: 'EN 12464-1:2021 Light and Lighting - Indoor Work Places',
-    url: 'https://standards.cencenelec.eu/',
+    url: 'https://knowledge.bsigroup.com/products/light-and-lighting-lighting-of-work-places-indoor-work-places-2',
   },
 ];
